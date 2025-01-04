de994b5b084c8c1b2c64a7fc9b238b1660ae0484
wongchs <chyisiang8@gmail.com>
2025-01-04 12:28:57 +0800

    implement authentication storage and context for sign-in flow

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,14 +1,19 @@
 import useSignIn from "../../hooks/useSignIn";
 import SignInForm from "./SignInForm";
+import { useNavigate } from "react-router-native";
 
 const SignIn = () => {
   const [signIn] = useSignIn();
+  const navigate = useNavigate();
 
   const onSubmit = async (values) => {
     const { username, password } = values;
 
     try {
       const { data } = await signIn({ username, password });
+      if (data?.authenticate?.accessToken) {
+        navigate("/", { replace: true });
+      }
       console.log("debug", data);
     } catch (e) {
       console.log(e);
--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,13 +1,23 @@
-import { useMutation } from "@apollo/client";
+import { useMutation, useApolloClient } from "@apollo/client";
 import { AUTHENTICATE } from "../graphql/mutations";
+import useAuthStorage from "../hooks/useAuthStorage";
 
 const useSignIn = () => {
   const [mutate, result] = useMutation(AUTHENTICATE);
+  const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
 
   const signIn = async ({ username, password }) => {
-    return mutate({
+    const { data } = await mutate({
       variables: { username, password },
     });
+
+    if (data?.authenticate?.accessToken) {
+      await authStorage.setAccessToken(data.authenticate.accessToken);
+      apolloClient.resetStore();
+    }
+
+    return { data };
   };
 
   return [signIn, result];
